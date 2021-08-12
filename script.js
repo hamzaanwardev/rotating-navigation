1b624da786f3d5734e5e577e5098daa4b719df4f
Hamza Anwar <hamzaanwardev@gmail.com>
2021-08-13 00:59:36 +0500

    Added Comments in Html, Css & JavaScript

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
+
+// Variables
 const open = document.getElementById("open");
 const close = document.getElementById("close");
 const container = document.querySelector(".container");
 
 
+// adding show-nav class
 open.addEventListener ("click", () => container.classList.add("show-nav"));
-
+// removing show-nav class
 close.addEventListener ("click", () => container.classList.remove("show-nav"));
 
